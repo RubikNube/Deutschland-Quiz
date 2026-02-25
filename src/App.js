bd656387ae1388e781aefb9311b462999ff23176
RubikNube <roland@kiesendahl.de>
2026-02-25 19:56:34 +0100

    feat(map): basic open street map integration
    added basic react app with open street map integration

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function App() {
+	return (
+		<div style={{ height: "100vh" }}>
+			<MapContainer center={[51, 10]} zoom={6} style={{ height: "100%", width: "100%" }}>
+				<TileLayer
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					attribution="&copy; OpenStreetMap contributors"
+				/>
+			</MapContainer>
+		</div>
+	);
+}
+
+export default App;
